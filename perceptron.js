e256555aab8e8231bafc2dfd8134d59931c557bb
Hytalo Bassi <hytalo-bassi@protonmail.com>
2025-11-14 20:59:01 -0400

    fix: conserta treinamento do neurônio

--- perceptron.js
+++ perceptron.js
@@ -1,5 +1,9 @@
 class Perceptron {
   constructor(features) {
+    // propriedades padroes que necessitam ser definidas
+    this.bias = 0;
+    this.learningRate = 0.0001;
+
     this.updateFeatures(features);
   }
 
@@ -20,14 +24,23 @@ class Perceptron {
   }
 
   predict(...X) {
-    return X.map((_, i) => X[i] * this.weights[i]).reduce((m, n) => m + n) + bias;
+    return X.map((_, i) => X[i] * this.weights[i]).reduce((m, n) => m + n) + this.bias;
+  }
+
+  train(X, y) {
+    const res = this.output(...X);
+    const error = y - res
+
+    for (i = 0; i < X.length; i++)
+      this.weights[i] += this.learningRate * error * X[i];
+
+    this.bias += this.learningRate * error;
   }
 
   train(X, y, epochs = 1000) {
-    // Train the perceptron using gradient descent
     for (let epoch = 0; epoch < epochs; epoch++) {
       for (let i = 0; i < X.length; i++) {
-        const prediction = this.predict(X[i]);
+        const prediction = this.output(X[i]);
         const error = y[i] - prediction;
 
         this.weights[i] += this.learningRate * error * X[i];
